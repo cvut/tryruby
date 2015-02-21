cbee85eb9a06a353b1eb4d1bce1ccaffd64f5b37
Jakub Jirutka <jakub@jirutka.cz>
2015-02-21 16:58:09 +0100

    Move custom rspec matchers to support/matchers.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,10 @@
 require 'stringio'
 require 'tryruby'
 
+Dir['./spec/support/**/*.rb'].each { |file| require file }
+
 RSpec.configure do |c|
   c.color = true
   c.formatter = :documentation
   c.tty = true
-end
-
-# mtm @ http://stackoverflow.com/questions/6372763/rspec-how-do-i-write-a-test-that-expects-certain-output-but-doesnt-care-about
-RSpec::Matchers.define :match_stdout do |check|
-  @capture = nil
-
-  match do |block|
-    begin
-      stdout_saved = $stdout
-      $stdout      = StringIO.new
-      block.call
-    ensure
-      @capture     = $stdout
-      $stdout      = stdout_saved
-    end
-
-    @capture.string.match check
-  end
-
-  failure_message do
-    "expected to #{description}"
-  end
-  failure_message_when_negated do
-    "expected not to #{description}"
-  end
-  description do
-    "match [#{check}] on stdout [#{@capture.string}]"
-  end
-
-  def supports_block_expectations?
-    true
-  end
 end
